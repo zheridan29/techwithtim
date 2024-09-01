5cab3907e39a99489cb3bf7c401b707ed0439885
zheridan29 <ace.zheridan@gmail.com>
2024-09-01 14:09:48 +0800

    put and delete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,13 +20,39 @@ videos = {
 
 }
 
+# define a function for abort, if the video_id does not exist in our videos dictionary
+# 404 - does not exists
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video ID does not valid...")
+
+# define a function to test if a video exist in the dictionary using a video_id
+# 409 - conflict with running the request in the server
+def abort_if_video_id_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID...")
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
     
     def put(self, video_id):
+        abort_if_video_id_exists(video_id)
         args = video_put_args.parse_args()
-        return {video_id: args}
+        # This is how you store data from the forms tru the args
+        # from the request data is save to the empty dictionary named: videos
+        videos[video_id] = args
+        # returns a web status code
+        return videos[video_id], 201
+    
+    # 204 - meaning deleted successfully
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
